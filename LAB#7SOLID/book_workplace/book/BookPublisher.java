abd4c649cdd833e69cfe989a8f35de872caeb5d6
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-03-05 17:28:48 +0700

    i hear too

--- LAB#7SOLID/book_workplace/book/BookPublisher.java
+++ LAB#7SOLID/book_workplace/book/BookPublisher.java
@@ -3,8 +3,8 @@ import java.util.List;
 
 public class BookPublisher implements PublisherServiceInterface {
     public static void main(String[] args) {
-        Book book2 = new Book("Tyland", List.of("I", "dont", "here", "recently", "know"));
-        // PublisherServiceInterface.printToFile(book2);
+        Book book2 = new Book("Tyland", List.of("I", "dont", "here", "recently", "too"));
+        PublisherServiceInterface.printToFile(book2);
         String title = book2.getTitle();
         System.out.println(title);
     }
