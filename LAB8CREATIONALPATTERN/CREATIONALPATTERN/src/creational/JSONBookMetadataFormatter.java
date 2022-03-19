01ad718b08607238a4535864c3dd110b069b5988
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-03-19 18:49:15 +0700

    add java project

--- LAB8CREATIONALPATTERN/CREATIONALPATTERN/src/creational/JSONBookMetadataFormatter.java
+++ LAB8CREATIONALPATTERN/CREATIONALPATTERN/src/creational/JSONBookMetadataFormatter.java
@@ -17,6 +17,6 @@ public class JSONBookMetadataFormatter implements BookMetadataFormatter {
     @Override
     public String getMetadataString() {
         // Please implement this method. You may create additional methods as you see fit.
-        return null;
+        return null; 
     }
 }
