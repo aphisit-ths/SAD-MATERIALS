eb988108459724a7495756aeba2f802a950b11af
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-03-25 03:45:35 +0700

    refactor

--- LAB8CREATIONALPATTERN/CREATIONALPATTERN/src/creational/BookMetadataFormatter.java
+++
@@ -1,7 +0,0 @@
-package creational;
-// DO NOT MODIFY ************************************************************
-public interface BookMetadataFormatter {
-    BookMetadataFormatter reset();
-    BookMetadataFormatter append(Book b);
-    String getMetadataString();
-}
